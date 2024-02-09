d61ed73f5804e8e4d90b536ae0cf52a8552c38eb
Siddharth Subramanian <sidsbrmnn@gmail.com>
2024-02-08 22:50:38 -0700

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Siddharth Subramanian on 2/8/24.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
